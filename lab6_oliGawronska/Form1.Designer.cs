fcda79ddf5383040b520fe949cce11262ad25e43
olimpia-windows <ogawronskao@gmail.com>
2025-04-10 09:54:46 +0200

    Dodaj pliki projektów.

---
+++ lab6_oliGawronska/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace lab6_oliGawronska
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.settings = new System.Windows.Forms.Button();
+            this.end = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(340, 201);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(133, 58);
+            this.start.TabIndex = 0;
+            this.start.Text = "Start";
+            this.start.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.start.UseVisualStyleBackColor = true;
+            // 
+            // settings
+            // 
+            this.settings.Location = new System.Drawing.Point(340, 286);
+            this.settings.Name = "settings";
+            this.settings.Size = new System.Drawing.Size(133, 60);
+            this.settings.TabIndex = 1;
+            this.settings.Text = "Ustawienia";
+            this.settings.UseVisualStyleBackColor = true;
+            // 
+            // end
+            // 
+            this.end.Location = new System.Drawing.Point(340, 373);
+            this.end.Name = "end";
+            this.end.Size = new System.Drawing.Size(136, 47);
+            this.end.TabIndex = 2;
+            this.end.Text = "Koniec";
+            this.end.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(317, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "GDZIE JEST DYDLEF";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.end);
+            this.Controls.Add(this.settings);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button settings;
+        private System.Windows.Forms.Button end;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ lab6_oliGawronska/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("lab6_oliGawronska")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab6_oliGawronska")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("49761dac-32cd-4824-8a2d-6ba4b0589529")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
